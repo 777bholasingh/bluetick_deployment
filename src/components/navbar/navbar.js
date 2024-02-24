e285fe4f3d019a80912a0dddcac7afbe4284a190
Bhola Singh <141518067+777bholasingh@users.noreply.github.com>
2024-02-24 09:47:46 +0530

    Updated

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -9,6 +9,9 @@ export default function Navbar() {
    <>
 <nav className={ `navbar navbar-expand-lg navbar-light bg-light` } id="navbr">
   <div className={`container-fluid `}>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                 <span className="navbar-toggler-icon"></span>
+                </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
